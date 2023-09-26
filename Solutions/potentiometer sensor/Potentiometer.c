0988719b7a46c86804463327b6b9a95b080be730
RubenW556 <ruben.wijnbergen@live.nl>
2023-09-26 11:53:15 +0200

    Added Potentiometer, sampling rate to ping

---
+++ Solutions/potentiometer sensor/Potentiometer.c
@@ -0,0 +1,68 @@
+#define POTENTIOPIN 7
+#define sensorUseage 2 //0=sampling rate 1=range 2=graphdata
+#define measurementAttempts 200
+
+
+#include "simpletools.h"                    
+#include "mstimer.h"
+#include "ping.h"
+
+//reference document https://learn.parallax.com/tutorials/language/blocklyprop/circuit-practice-blocklyprop/potentiometer-position
+
+long measurePotentiometer(){
+    high(POTENTIOPIN);
+    pause(1);
+    return rc_time(POTENTIOPIN, 1);
+  }
+  
+void measureRange(){
+    while(1){
+      printf("%ld \n", measurePotentiometer());
+      }
+  }  
+
+void createGaphData(){
+   mstime_start();
+  for(int i = -25; i<measurementAttempts; i++){ 
+      printf("%d, %d, %ld \n",i ,  mstime_get(), measurePotentiometer());
+    }
+  }
+  
+void measureSamplingrate(){
+  pause(500);
+  printf("start within 0.2s \n");
+  pause(200);
+
+  long currentMeasurement = -1; // the last measurement the device made
+  int changeCounter = 0; // counter for the amount of measurements that differ from the last one.
+  mstime_start();
+ 
+  for(int i = 0; i<measurementAttempts; i++){ 
+    long newMeasurement = measurePotentiometer();
+    if(newMeasurement!=currentMeasurement){
+      changeCounter++;
+      currentMeasurement=newMeasurement;
+      } 
+    pause(1); 
+  }  
+  int mstime = mstime_get();
+  printf("it took %dms \n", mstime);
+  printf("had %d changes \n", changeCounter);
+  printf("ESR is %.0f/s", ((float)changeCounter)/((float)mstime)*1000); // changes/time(MS) = sampling rate MS, sampling rate MS*1000 = sampling rate S
+}
+
+
+int main()                                    // Main function
+{
+  if(sensorUseage==0){
+    pause(1000);
+    measureSamplingrate();
+  }
+  else if(sensorUseage==1){
+    measureRange();
+  }
+   else if(sensorUseage==2){
+    createGaphData();
+  }
+   exit(0);
+}
--- Solutions/ultra sound sensor/opd3_ultra_sound_sensor.c
+++ Solutions/ultra sound sensor/opd3_ultra_sound_sensor.c
@@ -1,5 +1,5 @@
 #define PINGPIN 8
-#define measurementAttempts 1000
+#define measurementAttempts 200
 #define POTENTIOPIN 7
 
 #include "simpletools.h"                      // Include simple tools
@@ -27,11 +27,22 @@ int main()                                    // Main function
   printf("start");
   pause(50);
 
-  int currentMeasurement = -1; // the last measurement the device made
+  float currentMeasurement = -1; // the last measurement the device made
   int changeCounter = 0; // counter for the amount of measurements that differ from the last one.
   mstime_start();
+
  
   for(int i = 0; i<measurementAttempts; i++){ 
-    print("%d ,%.2f\n",mstime_get(), pingCmDistanceFloat());
-  }  
+    float newMeasurement = pingCmDistanceFloat();
+     
+    print("%d ,%.2f\n",mstime_get(), newMeasurement);
+    if(newMeasurement!=currentMeasurement){
+        changeCounter++;
+        currentMeasurement=newMeasurement;
+    }
+  }
+  int mstime = mstime_get();
+  printf("it took %dms \n", mstime);
+  printf("had %d changes \n", changeCounter);
+  printf("ESR is %.0f/s", ((float)changeCounter)/((float)mstime)*1000);  
 }
